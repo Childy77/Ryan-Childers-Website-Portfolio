093855921f7baf71185e5090050b6fec75e633f7
Ryan <ryanbairdchilders@gmail.com>
2023-04-04 11:40:26 -0700

    moved files

---
+++ src/components/about-me.js
@@ -0,0 +1,18 @@
+import React from "react";
+import headshot from "../assets/Headshot1.jfif";
+
+const AboutMe = () => {
+    return(
+        <section className="main-content" id="about-me">
+        
+        <img src={headshot} alt="A picture of Ryan" class="headshot"/>
+
+        <p>
+            Ryan Childers is an aspiring coder.  He spent 20 years as a professional actor and intructor, but now wants to pursue a life with a better pay schedule to fit his family needs.
+        </p>
+
+        </section>
+    )
+}
+
+export default AboutMe
