1e2b744d2247e6c70f97698477ac849e28035f6b
Ryan <ryanbairdchilders@gmail.com>
2023-04-24 11:47:49 -0700

    added email Link and icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,16 +5,20 @@ const Footer = () => {
         <>
         <section className="bottom-info" id="contact">
         <h2>425-293-4506</h2>
-        <h2>ryanbairdchilders@gmail.com</h2>
+        <h2>
+        <a href = "ryanbairdchilders@gmail.com">ryanbairdchilders@gmail.com</a>
+        </h2>
         <h2>
             <a href="https://github.com/Childy77">
             <i className="fa-brands fa-github"></i>Github/Childy77</a>
             </h2>
         <h2>
-        <a href="https://www.linkedin.com/in/ryan-childers-545853270/">linkedin</a>
+        <a href="https://www.linkedin.com/in/ryan-childers-545853270/">
+        <i class="fa-brands fa-linkedin"></i>linkedin</a>
         </h2>
         <h2>
-        <a href="https://docs.google.com/document/d/1_R8-1EVNYQDReYk4vAyLW7H1mvukf2OX9r5Wj8t56gI/edit">Resume</a>
+        <a href="https://docs.google.com/document/d/1_R8-1EVNYQDReYk4vAyLW7H1mvukf2OX9r5Wj8t56gI/edit">
+        <i class="fa-solid fa-file"></i>Resume</a>
         </h2>
     
     
